85e1b91bd1f2463d3351ad417642fb08133dad1e
juanchuletas <gamj@xanum.uam.mx>
2020-01-28 09:20:41 -0600

    Add

--- main.cpp
+++ main.cpp
@@ -7,7 +7,11 @@ int main()
 	s.AppendToList(10);
 	s.AppendToList(11);
 	s.PrependToList(21);
+	s.getSize();
 	s.ShowList();
+	cout<<"List Size: "<<s.getSize()<<"\n";
+	cout<<"List Size: "<<s.getSize()<<"\n";
+
 
 
 
